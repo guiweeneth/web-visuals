c9118485be73f1a48407e93fbaac256231978d5e
Guiweeneth <ana.arrme@gmail.com>
2020-06-04 13:05:24 +0200

    Index y gitignore

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Mi portfolio</title>
+        <link rel="stylesheet" href="css/style.css" />
+    </head>
+    <body>
+        <div id="container">
+            <div id="cabecera">
+                <h1>Título de la cabecera</h1>
+            </div>
+            <div id="post">
+                <h1>Este es el título del post</h1>
+                <p>Y este es un párrafo de ejemplo para pruebas.</p>
+                <p>Este es otro párrafo del blog, muy interesante</p>
+            </div>
+        </div>
+    </body>
+</html>
