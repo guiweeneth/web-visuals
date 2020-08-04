fb65ed605b262ccb2bf410c104ac1dd1dd36403f
Guiweeneth <ana.arrme@gmail.com>
2020-08-04 17:19:07 +0200

    figcaption fixed and aligned

--- views/asymetric_galery.php
+++ views/asymetric_galery.php
@@ -29,89 +29,89 @@
         <div id="container">
             <section id="asymetric-gallery">
 				<header>
-					<h1>Galería asimétrica y flexible</h1>
+					<h1>Flexible & asymetric gallery</h1>
 				</header>
 				<article>
 					<a href="" target="_blank">
 					<figure>
-						<img src="img/1.jpg" /> <figcaption>Planta 1</figcaption>
+						<img src="img/1.jpg" /> <figcaption>Plant 1</figcaption>
 					</figure>
 					</a>
 				</article>
 				<article>
 					<a href="" target="_blank">
 					<figure>
-						<img src="img/2.jpg" /> <figcaption>Planta 2</figcaption>
+						<img src="img/2.jpg" /> <figcaption>Plant 2</figcaption>
 					</figure>
 					</a>
 				</article>
 				<article>
 					<a href="" target="_blank">
 					<figure>
-						<img src="img/3.jpg" /> <figcaption>Planta 3</figcaption>
+						<img src="img/3.jpg" /> <figcaption>Plant 3</figcaption>
 					</figure>
 					</a>
 				</article>
 				<article>
 					<a href="" target="_blank">
 					<figure>
-						<img src="img/4.jpg" /> <figcaption>Planta 4</figcaption>
+						<img src="img/4.jpg" /> <figcaption>Plant 4</figcaption>
 					</figure>
 					</a>
 				</article>
 				<article>
 					<a href="" target="_blank">
 					<figure>
-						<img src="img/5.jpg" /> <figcaption>Planta 5</figcaption>
+						<img src="img/5.jpg" /> <figcaption>Plant 5</figcaption>
 					</figure>
 					</a>
 				</article>
 				<article>
 					<a href="" target="_blank">
 					<figure>
-						<img src="img/6.jpg" /> <figcaption>Planta 6</figcaption>
+						<img src="img/6.jpg" /> <figcaption>Plant 6</figcaption>
 					</figure>
 					</a>
 				</article>
 				<article>
 					<a href="" target="_blank">
 					<figure>
-						<img src="img/7.jpg" /> <figcaption>Planta 7</figcaption>
+						<img src="img/7.jpg" /> <figcaption>Plant 7</figcaption>
 					</figure>
 					</a>
 				</article>
 				<article>
 					<a href="" target="_blank">
 					<figure>
-						<img src="img/8.jpg" /> <figcaption>Planta 8</figcaption>
+						<img src="img/8.jpg" /> <figcaption>Plant 8</figcaption>
 					</figure>
 					</a>
 				</article>
 				<article>
 					<a href="" target="_blank">
 					<figure>
-						<img src="img/9.jpg" /> <figcaption>Planta 9</figcaption>
+						<img src="img/9.jpg" /> <figcaption>Plant 9</figcaption>
 					</figure>
 					</a>
 				</article>
 				<article>
 					<a href="" target="_blank">
 					<figure>
-						<img src="img/10.jpg" /> <figcaption>Planta 10</figcaption>
+						<img src="img/10.jpg" /> <figcaption>Plant 10</figcaption>
 					</figure>
 					</a>
 				</article>
 				<article>
 					<a href="" target="_blank">
 					<figure>
-						<img src="img/11.jpg" /> <figcaption>Planta 11</figcaption>
+						<img src="img/11.jpg" /> <figcaption>Plant 11</figcaption>
 					</figure>
 					</a>
 				</article>
 				<article>
 					<a href="" target="_blank">
 					<figure>
-						<img src="img/12.jpg" /> <figcaption>Planta 12</figcaption>
+						<img src="img/12.jpg" /> <figcaption>Plant 12</figcaption>
 					</figure>
 					</a>
 				</article>
